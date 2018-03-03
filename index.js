b8d2ffee78fe791a3028823d6e866c8e87abead7
Mitch Ruebush <mitchruebush@Mitchs-MacBook-Pro.local>
2018-03-03 09:00:31 -0500

    some basic requests

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ var server = restify.createServer();
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
+server.get('/', function(req, res, next) {
+    res.send('home')
+    return next();
+});
+  
+server.post('/foo', function(req, res, next) {
+      req.someData = 'foo';
+      return next();
+    },
+    function(req, res, next) {
+      res.send(req.someData);
+      return next();
+    }
+);
+
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
